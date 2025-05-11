517a92e56d1db2d2243dd4176e8d3d5defb06bad
tejashChouhan999 <tejashchouhan23@gmail.com>
2025-05-11 21:20:08 +0530

    Delete script.js
    H

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('downloadBtn').addEventListener('click', function() {
-    const url = document.getElementById('videoUrl').value;
-    if (url) {
-        document.getElementById('statusMessage').textContent = 'Download started from InstaSaverHD';
-    } else {
-        document.getElementById('statusMessage').textContent = 'Please enter a valid URL.';
-    }
-});
